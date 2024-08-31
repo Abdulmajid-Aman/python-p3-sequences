0044ed21ba56907ce1fed2c242f4bd12d6af40ab
Abdulmajid Aman Hussein <abdulmajid.aman.student@moringaschool.com>
2024-08-31 17:33:58 +0300

    finobacci sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    elif length == 2:
+        print([0, 1])
+    else:
+        a = 0
+        b = 1
+        sequence = [a, b]
+        for i in range(2, length):
+            c = a + b
+            a = b
+            b = c
+            sequence.append(b)
+        print(sequence)
